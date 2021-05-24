3453857903077d76a5727d44705a3a7d851a8880
卢健 <lujian@prolog-int.com>
2021-05-24 15:19:55 +0800

    添加test

--- microservice-common/src/main/java/com/itheima/model/User.java
+++ microservice-common/src/main/java/com/itheima/model/User.java
@@ -1,9 +1,12 @@
 package com.itheima.model;
 
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
 import javax.persistence.Table;
+import java.util.Date;
 
 @Data
 public class User {
@@ -11,4 +14,16 @@ public class User {
     private String name;
     private int age;
     private String email;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 最后修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
 }
--- microservice-pluservice/src/test/java/com/lujian/microservicepluservice/test/Test.java
+++ microservice-pluservice/src/test/java/com/lujian/microservicepluservice/test/Test.java
@@ -5,7 +5,9 @@ import com.lujian.microservicepluservice.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @SpringBootTest
 public class Test {
@@ -13,12 +15,35 @@ public class Test {
     @Autowired
     private UserMapper userMapper;
 
-    @org.junit.jupiter.api.Test
+   // @org.junit.jupiter.api.Test
     public void testSelect() {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
         for(User user:userList) {
             System.out.println(user);
+        }
+
+        Map<String, Object> columnMap=new HashMap<>();
+        columnMap.put("id",1);
+        List<User> users=  userMapper.selectByMap(columnMap);
+        users.stream().forEach(user -> {
+            System.out.println(user);
+        });
+    }
+
+    @org.junit.jupiter.api.Test
+    public void testAutoFill() {
+        User user = new User();
+        user.setName("tom");
+        user.setAge(20);
+        user.setEmail("tom@163.com");
+        if (userMapper.insert(user)==1) {
+            List<User> userList = userMapper.selectList(null);
+            for(User user1:userList) {
+                System.out.println(user1);
+            }
+        } else {
+            System.out.println("添加数据失败");
         }
     }
 }
