944d1068fc2ee49be8c30ed05f637693846c348b
卢健 <lujian@prolog-int.com>
2021-06-04 17:23:22 +0800

    mybaitplus

---
+++ microservice-pluservice/src/main/java/com/lujian/microservicepluservice/service/UserService.java
@@ -0,0 +1,7 @@
+package com.lujian.microservicepluservice.service;
+
+import com.itheima.model.User;
+
+public interface UserService {
+    void saveUser(User user);
+}
--- microservice-pluservice/src/test/java/com/lujian/microservicepluservice/test/Test.java
+++ microservice-pluservice/src/test/java/com/lujian/microservicepluservice/test/Test.java
@@ -2,6 +2,8 @@ package com.lujian.microservicepluservice.test;
 
 import com.itheima.model.User;
 import com.lujian.microservicepluservice.mapper.UserMapper;
+import com.lujian.microservicepluservice.service.UserService;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,10 +12,14 @@ import java.util.List;
 import java.util.Map;
 
 @SpringBootTest
+@Log4j2
 public class Test {
 
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    private UserService userService;
 
    // @org.junit.jupiter.api.Test
     public void testSelect() {
@@ -37,13 +43,24 @@ public class Test {
         user.setName("tom");
         user.setAge(20);
         user.setEmail("tom@163.com");
-        if (userMapper.insert(user)==1) {
-            List<User> userList = userMapper.selectList(null);
-            for(User user1:userList) {
-                System.out.println(user1);
-            }
-        } else {
-            System.out.println("添加数据失败");
-        }
+//        if (userMapper.insert(user)==1) {
+//            List<User> userList = userMapper.selectList(null);
+//            for(User user1:userList) {
+//                System.out.println(user1);
+//            }
+//        } else {
+//            System.out.println("添加数据失败");
+//        }
+        userService.saveUser(user);
     }
+
+//    public static void main(String[] args) {
+//        StringBuilder stringBuilder=new StringBuilder();
+//        stringBuilder.append("张三");
+//        stringBuilder.append("李四");
+//        String str="张三";
+//        str=str+"李四";
+//        log.info(stringBuilder);
+//        log.info(str);
+//    }
 }
