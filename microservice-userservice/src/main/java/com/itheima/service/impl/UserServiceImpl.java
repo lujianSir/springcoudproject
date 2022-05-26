580c9d9de3b6901a3cb5c5995ba6f4928eabeee6
卢健 <lujian@prolog-int.com>
2022-05-26 16:54:20 +0800

    ceshi

--- microservice-userservice/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ microservice-userservice/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -41,7 +41,7 @@ public class UserServiceImpl implements UserService {
         userModel.setOrderList(restMessage.getData());
         System.out.println(redisUtil.get("username"));
 
-        String msg = "hello fanout =========================";
+        String msg = "hello fanout 你好=========================";
         try {
             //将消息携带绑定键值：TestDirectRouting 发送到交换机TestDirectExchange
             rabbitTemplate.convertAndSend("TestDirectExchange1", "TestDirectRouting", msg);
