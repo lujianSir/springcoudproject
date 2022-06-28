6aedddd06937660acd1955fec22d328443271fc7
卢健 <lujian@prolog-int.com>
2022-06-28 16:59:47 +0800

    解决报错

--- microservice-common/src/main/java/com/itheima/model/User.java
+++ microservice-common/src/main/java/com/itheima/model/User.java
@@ -1,11 +1,8 @@
 package com.itheima.model;
 
 
-import com.baomidou.mybatisplus.annotation.FieldFill;
-import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
-import javax.persistence.Table;
 import java.util.Date;
 
 @Data
@@ -28,12 +25,10 @@ public class User {
     /**
      * 创建时间
      */
-    @TableField(fill = FieldFill.INSERT)
     private Date createTime;
 
     /**
      * 最后修改时间
      */
-    @TableField(fill = FieldFill.INSERT_UPDATE)
     private Date updateTime;
 }
